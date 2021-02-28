cf802855d6ae758ac9d3af356d02c188b223a2f0
joanQz <joan@ereasons.com>
2021-02-28 19:50:05 +0100

    (feature) Complete API
    Adding "limits" parameter and Observable as return type

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-export function compress() {
-  console.log('executed nothing');
-}
+import { Observable } from 'rxjs';
 
 export class Reweight {
 
-  static compressImage(image: File) {
+  static compressImage(image: File, limits: {
+        maxImageSize?: number,
+        jpegQuality?: number,
+        maxFileSizeMb?: number,
+        imageSizeRatio?: number,
+        jpegQualityRatio?: number
+  }): Observable<File> {
+    let obs = new Observable<File>();
 
+    return obs;
   }
 
   static getUrlFromBlob(file: File): string {
