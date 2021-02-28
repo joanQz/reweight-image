5554b40f17da8153c7e8133a5d8343ed46533b5a
joanQz <joan@ereasons.com>
2021-02-28 19:02:15 +0100

    (feature) Add API used in origin (mqr app)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 export function compress() {
   console.log('executed nothing');
 }
+
+export class Reweight {
+
+  static compressImage(image: File) {
+
+  }
+
+  static getUrlFromBlob(file: File): string {
+    return '';
+  }
+}
