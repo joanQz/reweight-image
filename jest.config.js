3c5ddd0510a69fdd7475bba0a747d034a05b46e2
joanQz <joan@ereasons.com>
2021-03-01 14:31:38 +0100

    (test) Add test for verifying jest is working with Typescript

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@
  */
 
 module.exports = {
-  preset: "ts-jest"
+  preset: "ts-jest",
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
---
+++ test/index.test.ts
@@ -0,0 +1,6 @@
+const reweight = require('../src/index.ts');
+
+test('jest should be working with Typescript', ()=>{
+  let n: number = 2;
+  expect(n).toBe(2);
+});
