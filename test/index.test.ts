94e04dc7a1a13f168cbd10d3aa2c16efd0ef7835
joanQz <joan@ereasons.com>
2021-03-03 11:29:10 +0100

    (fix) Fix getScale testing (old was before keept)
    In corresponding refactoring, testing was not done

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,31 +5,25 @@ test('jest should be working with Typescript', ()=>{
   expect(n).toBe(2);
 });
 
-test('get scales for 4000x3000 to max 1000 cover returns {0.33, 1333, 1000}', ()=>{
+test('get scale for 4000x3000 to max 1000 cover returns 0.33', ()=>{
   const reweighter = new reweight.Reweight();
-  let getScales = reweighter['getScales'];
-  const {scale: scale, xScale: xScale, yScale: yScale } = getScales(1000, 4000, 3000, true);
+  let getScale = reweighter['getScale'];
+  const scale = getScale(1000, 4000, 3000, true);
   expect(scale).toBe(1/3);
-  expect(xScale).toBe(4000/3);
-  expect(yScale).toBe(1000);
 });
 
-test('get scales for 4000x3000 to max 5000 cover returns {1, 4000, 3000}', ()=>{
+test('get scale for 4000x3000 to max 5000 cover returns 1', ()=>{
   const reweighter = new reweight.Reweight();
-  let getScales = reweighter['getScales'];
-  const {scale: scale, xScale: xScale, yScale: yScale } = getScales(5000, 4000, 3000, true);
+  let getScale = reweighter['getScale'];
+  const scale = getScale(5000, 4000, 3000, true);
   expect(scale).toBe(1);
-  expect(xScale).toBe(4000);
-  expect(yScale).toBe(3000);
 });
 
-test('get scales for 4000x3000 to max 1000 not cover returns {0.25, 1000, 750}', ()=>{
+test('get scale for 4000x3000 to max 1000 not cover returns 0.25', ()=>{
   const reweighter = new reweight.Reweight();
-  let getScales = reweighter['getScales'];
-  const {scale: scale, xScale: xScale, yScale: yScale } = getScales(1000, 4000, 3000, false);
+  let getScale = reweighter['getScale'];
+  const scale = getScale(1000, 4000, 3000, false);
   expect(scale).toBe(0.25);
-  expect(xScale).toBe(1000);
-  expect(yScale).toBe(750);
 });
 
 // TODO: test main function with a headless browser
